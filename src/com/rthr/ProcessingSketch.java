77d4353725fcf3b20c12cd03f3f2e79706ca144c
Arthur Lockman <hello@rthr.me>
2014-03-20 12:11:26 -0400

    Initial Commit

---
+++ src/com/rthr/ProcessingSketch.java
@@ -0,0 +1,20 @@
+package com.rthr;
+import processing.core.*;
+
+public class ProcessingSketch extends PApplet
+{
+	public static void main(String args[])
+	{
+		PApplet.main("com.rthr.ProcessingSketch");
+	}
+
+	public void setup()
+	{
+		size(200,200);
+	}
+
+	public void draw()
+	{
+
+	}
+}
